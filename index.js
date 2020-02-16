86ea7dbf840f11a50eba240a49875795f904c3be
Luis Pestana <lpestana@protonmail.com>
2020-02-16 01:00:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const shout = string => string.toUpperCase();
+
+const whisper = string => string.toLowerCase();
+
+const logShout = string => console.log(shout(string));
+
+const logWhisper = string => console.log(whisper(string));
