924b2321f2ac33799be2436624cea0fba203661d
Luis Pestana <lpestana@protonmail.com>
2020-02-16 01:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const whisper = string => string.toLowerCase();
 const logShout = string => console.log(shout(string));
 
 const logWhisper = string => console.log(whisper(string));
+
+const sayHiToGrandma = string => {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return 'YES INDEED!';
+  } else if (string === 'I love you, Grandma') {
+    return 'I love you, too.';
+  }
+}
